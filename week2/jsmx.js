c628ff235cf3c780b9f58c61220583f46aaf7105
balib01 <balib1437@gmail.com>
2020-10-21 23:19:41 +0400

    content modified

--- week2/jsmx.js
+++ week2/jsmx.js
@@ -1,5 +1,6 @@
 const classToggle = () => {
-  const navs = document.getElementById('section')
+  const navs = document.querySelector('.section')
   navs.classList.toggle('nav-toggleShow');
+  console.log(nav.classlost);
 }
-document.querySelector('.hamburger').addEventListener('click', classToggle);
+document.querySelector('.hamburger').addEventListener('click', classToggle); 
