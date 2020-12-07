b23188c7f3ae8daef81525a4cb4be73c811a3510
balib01 <balib1437@gmail.com>
2020-12-07 16:51:28 +0400

    update

--- pluralsight-projects-Vue-BookListing-e970e5c/src/components/BookList.vue
+++ pluralsight-projects-Vue-BookListing-e970e5c/src/components/BookList.vue
@@ -1,6 +1,10 @@
 <template>
 <div>
   <h1>{{title}}</h1>
+  <ul>
+    <li v-for='book in books' : book='book'>{{book.title}}:{{book.author}}</li>
+    
+  </ul>
 </div>
 </template>
 
@@ -11,6 +15,9 @@ export default {
    data() {
      return {
        title: 'All Books',
+       books:[ {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+    {title: 'American Gods', author: 'Neil Gaiman'},
+    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},]
     }
    }
 }
@@ -19,5 +26,13 @@ export default {
 <style>
 h1,h2 {
   font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
 }
 </style>
