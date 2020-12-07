bff346eb2de5f2e9038cfcd2442e7d92c9acfd9d
balib01 <balib1437@gmail.com>
2020-12-07 17:10:19 +0400

    new module

---
+++ pluralsight-projects-Vue-BookListing-e970e5c/src/components/BookItem.vue
@@ -0,0 +1,21 @@
+<template>
+<ul>
+  <li>{{book.title}}:{{book.author}}</li>
+</ul>
+</template>
+<script>
+  export default {
+    name: 'BookItem',
+    props: ['book'],
+  }
+</script>
+<style lang="stylus" scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
