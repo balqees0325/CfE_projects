aac3fe0aa186242c3109a12ea74b6d3743e5e85f
balib01 <balib1437@gmail.com>
2020-12-07 14:19:58 +0400

    files added

---
+++ pluralsight-projects-Vue-BookListing-e970e5c/src/components/BookList.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <h1>{{title}}</h1>
+</div>
+</template>
+
+
+<script>
+export default {
+   name: 'BookList',
+   data() {
+     return {
+       title: 'All Books',
+    }
+   }
+}
+</script>
+
+<style>
+h1,h2 {
+  font-weight: normal;
+}
+</style>
