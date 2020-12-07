af0fe6094cec0e9812d015a1ee67d8ed594aeda2
balib01 <balib1437@gmail.com>
2020-12-08 03:36:58 +0400

    modify

--- pluralsight-projects-Vue-BookListing-e970e5c/src/components/BookForm.vue
+++ pluralsight-projects-Vue-BookListing-e970e5c/src/components/BookForm.vue
@@ -1,5 +1,5 @@
 <template>
-     <form onsubmit.prevent ="bookSubmit(bookData);" class="" action="#" method="post">
+     <form v-on:submit.prevent ="bookSubmit(bookData);" class="" action="#" method="post">
     <input v-model='bookData.bookTitle' type="text" name="title" value="" placeholder="Book Title">
     <input 
    v-model='bookData.bookAuthor' type="text" name="author" value="" placeholder="Book Author">
