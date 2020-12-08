e382e62a2d6d6ae84abcf6a0492f2b2807b4f40d
balib01 <balib1437@gmail.com>
2020-12-08 12:12:13 +0400

    modify

--- pluralsight-projects-Vue-BookListing-Forms-8ff0412/src/components/BookList.vue
+++ pluralsight-projects-Vue-BookListing-Forms-8ff0412/src/components/BookList.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <input v-model="searchInput" type="text" placeholder="Search Books">
     <ul>
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+      <book-item v-for='book in searchedBooks' :key='book.id' :book='book'></book-item>
     </ul>
     <hr>
     <h2>Filtered Books By Ownership</h2>
@@ -41,13 +42,20 @@ export default {
         ownership:'borrowed'}
       ],
       filters: ['bought', 'borrowed'],
-      holding: 'bought'
+      holding: 'bought',
+      searchInput: ''
     };
   },
   computed: {
     filteredBooks() {
     return _.filter(this.books, ["ownership", this.holding]);
-}
+},
+   searchedBooks() {
+     const searchFilter = book => {
+  return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+};
+return _.filter(this.books, searchFilter);
+   }
   },
   components: {
     BookItem,
